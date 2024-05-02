f29c2e6d6b72dd3713e19c5e92b7e0f1ae0374cb
ItsBlockFighter <tarrkrisztofer@gmail.com>
2024-05-03 00:01:46 +0200

    feat(auth): username and avatar update

--- app/src/main/java/hu/krisztofertarr/forum/service/AuthService.java
+++ app/src/main/java/hu/krisztofertarr/forum/service/AuthService.java
@@ -1,39 +1,81 @@
 package hu.krisztofertarr.forum.service;
 
+import android.net.Uri;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import hu.krisztofertarr.forum.util.Callback;
 
 public class AuthService {
 
     private static AuthService instance;
 
     public static AuthService getInstance() {
-        if(instance == null) {
+        if (instance == null) {
             instance = new AuthService();
         }
         return instance;
     }
 
-    private final FirebaseAuth firebaseAuth;
+    private final FirebaseAuth auth;
 
     private AuthService() {
-        this.firebaseAuth = FirebaseAuth.getInstance();
+        this.auth = FirebaseAuth.getInstance();
     }
 
     public boolean isLoggedIn() {
-        return firebaseAuth.getCurrentUser() != null;
+        return auth.getCurrentUser() != null;
     }
 
     public void logOut() {
-        firebaseAuth.signOut();
+        auth.signOut();
     }
 
-    public void register(String email, String password) {
-        firebaseAuth.createUserWithEmailAndPassword(email, password);
+    public Task<AuthResult> register(String email, String password) {
+        return auth.createUserWithEmailAndPassword(email, password);
     }
 
-    public void login(String email, String password) {
-        firebaseAuth.signInWithEmailAndPassword(email, password);
+    public Task<AuthResult> login(String email, String password) {
+        return auth.signInWithEmailAndPassword(email, password);
+    }
+
+    public FirebaseUser user() {
+        return auth.getCurrentUser();
     }
 
 
+    public Task<AuthResult> loginGuest() {
+        return auth.signInAnonymously();
+    }
+
+    public void updateUsername(String string, Callback<Void> callback) {
+        user().updateProfile(new UserProfileChangeRequest.Builder()
+                .setDisplayName(string)
+                .build())
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        callback.onSuccess(null);
+                    } else {
+                        callback.onFailure(task.getException());
+                    }
+                });
+    }
+
+    public void updateAvatar(Uri uri, Callback<Void> callback) {
+        user().updateProfile(new UserProfileChangeRequest.Builder()
+                .setPhotoUri(uri)
+                .build())
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        callback.onSuccess(null);
+                    } else {
+                        callback.onFailure(task.getException());
+                    }
+                });
+    }
 }
