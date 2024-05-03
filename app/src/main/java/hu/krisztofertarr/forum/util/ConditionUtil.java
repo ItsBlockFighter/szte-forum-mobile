32ab179acb0b1834790aeb0fd553714381b20e02
ItsBlockFighter <tarrkrisztofer@gmail.com>
2024-05-03 17:00:16 +0200

    feat: added auto toast in condition util

--- app/src/main/java/hu/krisztofertarr/forum/util/ConditionUtil.java
+++ app/src/main/java/hu/krisztofertarr/forum/util/ConditionUtil.java
@@ -1,14 +1,18 @@
 package hu.krisztofertarr.forum.util;
 
+import android.content.Context;
+import android.widget.Toast;
+
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class ConditionUtil {
 
 
-    public static void assertIsNotEmpty(String value, String message) {
+    public static void assertIsNotEmpty(Context context, String value, String message) {
         if(value == null || value.isEmpty()) {
             throw new IllegalArgumentException(message);
         }
+        Toast.makeText(context, value, Toast.LENGTH_SHORT).show();
     }
 }
